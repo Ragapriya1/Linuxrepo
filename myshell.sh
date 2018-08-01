dc42a1a4cc5c6fc6681f0196cee041ef7561f714
Ragapriya1 <meetraga@gmail.com>
2018-08-01 08:37:55 +0530

    shell.sh is committed

---
+++ myshell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+: 'Created for learning purpose by Ragapriya on 26/07/2018'
+
+#function execution example
+
+function fn1()
+{
+ls
+echo "welcome"
+}
+
+fn1
+
+: ' function addition(a,b)
+{
+c=a+b
+echo $c
+}
+
+addition(10,20)'
+
+
